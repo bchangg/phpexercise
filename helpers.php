e9a93cfc92f253a0f912340812b8eb9307c1e348
Brian Chang <changbhc@gmail.com>
2020-09-17 22:26:24 -0700

    add function to generate the HTML content

--- helpers.php
+++ helpers.php
@@ -13,4 +13,44 @@
 
     return $data;
   }
+
+  function constructPage($data) {
+    $contents = '';
+
+    foreach ($data as $entry) {
+      $contents .= <<<HTML
+      <tr>
+        <td>{$entry->userId}</td>
+        <td>{$entry->id}</td>
+        <td>{$entry->title}</td>
+        <td>{$entry->body}</td>
+      </tr>
+      HTML;
+    }
+
+    return <<<HTML
+      <!DOCTYPE html>
+        <html lang="en" dir="ltr">
+          <head>
+            <meta charset="utf-8">
+            <title></title>
+          </head>
+          <body>
+            <table>
+              <thead>
+                <tr>
+                  <th>User ID</th>
+                  <th>ID</th>
+                  <th>Title</th>
+                  <th>Body</th>
+                </tr>
+              </thead>
+              <tbody>
+                {$contents}
+              </tbody>
+            </table>
+          </body>
+        </html>
+    HTML;
+  }
 ?>
